05c43283015cdef84b23b7946cb723b11faa400b
tamarareginafumeiro <tamara.rfumeiro@senacsp.edu.br>
2024-02-23 22:14:40 -0300

    23/02/2024

---
+++ projeto_luana/JS/interacoes.js
@@ -0,0 +1,11 @@
+//Humburger
+function clickMenu(){
+
+    if(itens.style.display == 'block'){ //abrir
+        itens.style.display = 'none'
+    }else{
+        itens.style.display = 'block' //fechar
+}
+
+     
+}
